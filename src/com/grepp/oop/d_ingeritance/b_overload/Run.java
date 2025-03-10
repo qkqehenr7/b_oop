d0c29ae065d777f31c0d564260f4f14d0040e26b
Sunnyboy037 <qkqehenr37@gmail.com>
2025-03-10 14:27:04 +0900

    20250310

---
+++ src/com/grepp/oop/d_ingeritance/b_overload/Run.java
@@ -0,0 +1,11 @@
+package com.grepp.oop.d_ingeritance.b_overload;
+
+public class Run {
+
+    public static void main(String[] args) {
+        A_overloading overloading = new A_overloading();
+        double res = overloading.plus(10.0, 20.0, 1,2,3,4,5,6,7,8,9,11,13);
+        System.out.println(res);
+    }
+
+}
